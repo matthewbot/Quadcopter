ba9cec6400a8d3c2b2cb6f1455e215e24fee1dac
Matthew Thompson <matthewbot@gmail.com>
2009-07-17 04:12:35 +0000

    made system init the nvic

--- FC/src/system.c
+++ FC/src/system.c
@@ -3,11 +3,13 @@
 #include "drivers/stdio.h"
 #include "peripherals/gpio.h"
 #include "peripherals/rcc.h"
+#include "peripherals/nvic.h"
 #include <stdio.h>
 
 void system_init() {
 	rcc_init();
 	gpio_init();
+	nvic_init();
 	
 	panel_init();
 	stdio_init();
