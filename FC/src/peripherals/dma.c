8b6cf8181a941c4b6fb18d1885145d40a44fecd4
Matthew Thompson <matthewbot@gmail.com>
2009-07-17 01:38:51 +0000

    Wrote peripheral code for dma

---
+++ FC/src/peripherals/dma.c
@@ -0,0 +1,47 @@
+#include "peripherals/dma.h"
+#include "stm32f10x.h"
+#include <stdint.h>
+
+static DMA_Channel_TypeDef *const channels[] = { 
+	DMA1_Channel1, DMA1_Channel2, DMA1_Channel3, DMA1_Channel4, DMA1_Channel5, DMA1_Channel6, DMA1_Channel7
+};
+
+void dma_init() { }
+
+void dma_configure(int chnum, enum dma_direction dir, enum dma_priority pri, int memsize, int persize, enum dma_options opts) {
+	uint32_t val = opts;
+	
+	if (dir == DMA_DIR_MEM_TO_PERIPHERAL)
+		val |= DMA_CCR1_DIR;
+		
+	val |= pri << 12;	
+		
+	if (memsize == 16)
+		val |= DMA_CCR1_MSIZE_0;
+	else if (memsize == 32)
+		val |= DMA_CCR1_MSIZE_1;
+		
+	if (persize == 16)
+		val |= DMA_CCR1_PSIZE_0;
+	else if (memsize == 32)
+		val |= DMA_CCR1_MSIZE_1;
+		
+	channels[chnum]->CCR = val;
+}
+
+void dma_start(int chnum, void *mem, void *periph, unsigned short count) {
+	DMA_Channel_TypeDef *ch = channels[chnum];
+	ch->CCR &= ~DMA_CCR1_EN;
+	ch->CNDTR = count;
+	ch->CPAR = (uint32_t)periph;
+	ch->CMAR = (uint32_t)mem;
+	ch->CCR |= DMA_CCR1_EN;
+}
+
+void dma_disable(int chnum) {
+	channels[chnum]->CCR &= ~DMA_CCR1_EN;
+}
+
+void dma_enable(int chnum) {
+	channels[chnum]->CCR |= DMA_CCR1_EN;
+}
---
+++ FC/src/peripherals/dma.h
@@ -0,0 +1,37 @@
+#ifndef FC_PERIPHERALS_DMA_H
+#define FC_PERIPHERALS_DMA_H
+
+#include "stm32f10x.h"
+
+enum dma_interrupt_type {
+	DMA_TRANSFER_COMPLETE,
+	DMA_TRANSFER_HALF,
+	DMA_TRANSFER_ERROR
+};
+
+enum dma_priority {
+	DMA_PRIORITY_LOW = 0x00,
+	DMA_PRIORITY_MEDIUM = 0x01,
+	DMA_PRIORITY_HIGH = 0x02,
+	DMA_PRIORITY_VERY_HIGH = 0x03
+};
+
+enum dma_direction {
+	DMA_DIR_MEM_TO_PERIPHERAL,
+	DMA_DIR_PERIPHERAL_TO_MEM
+};
+
+enum dma_options {
+	DMA_OPTION_MEMORY_INCREMENT = DMA_CCR1_MINC,
+	DMA_OPTION_PERIPHERAL_INCREMENT = DMA_CCR1_PINC,
+	DMA_OPTION_CIRCULAR = DMA_CCR1_CIRC,
+	DMA_OPTION_INTERRUPT = DMA_CCR1_TCIE
+};
+
+void dma_init();
+void dma_configure(int chnum, enum dma_direction dir, enum dma_priority pri, int memsize, int persize, enum dma_options opts);
+void dma_start(int chnum, void *mem, void *periph, unsigned short count);
+void dma_disable(int chnum);
+void dma_enable(int chnum);
+
+#endif
