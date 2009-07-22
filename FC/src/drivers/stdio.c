e90c99a6dbf3ca61d2c0fd1669b3bef81531ee6b
Matthew Thompson <matthewbot@gmail.com>
2009-07-22 14:38:32 +0000

    Cleaned up code, lowered baud rate

--- FC/src/drivers/stdio.c
+++ FC/src/drivers/stdio.c
@@ -12,7 +12,6 @@
 
 #define BUF_SIZE 128
 
-/*
 static uint8_t readbuf[BUF_SIZE], writebuf[BUF_SIZE];
 
 static void readbuf_full_handler();
@@ -24,12 +23,10 @@ void stdio_init() {
 	dma_configure(DMA_TX, DMA_DIR_MEM_TO_PERIPHERAL, DMA_PRIORITY_LOW, 8, 8, DMA_OPTION_MEMORY_INCREMENT);
 	dma_configure(DMA_RX, DMA_DIR_PERIPHERAL_TO_MEM, DMA_PRIORITY_LOW, 8, 8, DMA_OPTION_MEMORY_INCREMENT | DMA_OPTION_INTERRUPT);
 	start_rx_dma(0);
-	usart_setup(1, 115200, true);
+	usart_setup(1, 9600, true);
 }
 
 size_t stdio_read(uint8_t *buf, size_t len) {
-	dma_disable(DMA_RX);
-	
 	size_t avail;
 	do {
 		avail = sizeof(readbuf) - dma_get_remaining(DMA_RX);
@@ -67,18 +64,4 @@ static void readbuf_full_handler() {
 	
 static void start_rx_dma(size_t bufinuse) {
 	dma_start(DMA_RX, &readbuf[bufinuse], usart_dma_address(1), sizeof(readbuf) - bufinuse);
-}
-*/
-	
-void stdio_init() {
-	usart_setup(1, 9600, false);
-}
-
-size_t stdio_read(uint8_t *buf, size_t len) {
-	return 0;
-}
-
-size_t stdio_write(const uint8_t *buf, size_t len) {
-	usart_send(1, buf, len);
-	return len;
 }
