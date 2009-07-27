12725f38a0e869322cc53a141b462c5183edf9dc
Matthew Thompson <matthewbot@gmail.com>
2009-07-27 02:20:58 +0000

    Enable hardware NSS pin on SPI

--- FC/src/peripherals/spi.c
+++ FC/src/peripherals/spi.c
@@ -7,13 +7,15 @@ static SPI_TypeDef *const spis[] = { 0, SPI1, SPI2 };
 void spi_setup(int num, enum spi_baud_div baud, bool dma) {
 	SPI_TypeDef *spi = spis[num];
 
-	spi->CR2 = (dma ? SPI_CR2_TXDMAEN | SPI_CR2_RXDMAEN : 0);	
+	spi->CR2 = SPI_CR2_SSOE | (dma ? SPI_CR2_TXDMAEN | SPI_CR2_RXDMAEN : 0);	
 	spi->CR1 = SPI_CR1_SPE | SPI_CR1_MSTR | (baud << 3);
 }
 
 void spi_disable(int num) {
 	spis[num]->CR1 &= ~SPI_CR1_SPE;
 }
+
+#include <stdio.h>
 
 void spi_send_receive(int num, uint8_t *data_in, const uint8_t *data_out, size_t len) {
 	SPI_TypeDef *spi = spis[num];
