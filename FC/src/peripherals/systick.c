16114a1716c7306a70bd1b6089dd455adad8d34e
Matthew Thompson <matthewbot@gmail.com>
2009-07-28 04:01:02 +0000

    Wrote function to read systick value

--- FC/src/peripherals/systick.c
+++ FC/src/peripherals/systick.c
@@ -7,3 +7,7 @@ void systick_setup(int microseconds) {
 	SysTick->LOAD = microseconds*72 - 1;
 	SysTick->CTRL = SysTick_CTRL_CLKSOURCE | SysTick_CTRL_TICKINT | SysTick_CTRL_ENABLE;
 }
+
+uint32_t systick_get() {
+	return SysTick->VAL;
+}
--- FC/src/peripherals/systick.h
+++ FC/src/peripherals/systick.h
@@ -1,6 +1,9 @@
 #ifndef FC_PERIPHERALS_SYSTICK_H
 #define FC_PERIPHERALS_SYSTICK_H
 
+#include <stdint.h>
+
 void systick_setup(int microseconds); // number of microseconds till interrupt
+uint32_t systick_get();
 
 #endif
