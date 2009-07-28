b7a6016545966410346a653dff50a4c0e6846fde
Matthew Thompson <matthewbot@gmail.com>
2009-07-28 03:25:16 +0000

    Corrected systick timing calculation

--- FC/src/peripherals/systick.c
+++ FC/src/peripherals/systick.c
@@ -4,6 +4,6 @@
 // why SysTick? why not STK? :/
 
 void systick_setup(int microseconds) {
-	SysTick->LOAD = (microseconds-1)*72;
+	SysTick->LOAD = microseconds*72 - 1;
 	SysTick->CTRL = SysTick_CTRL_CLKSOURCE | SysTick_CTRL_TICKINT | SysTick_CTRL_ENABLE;
 }
