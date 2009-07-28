2382dd45819f4d2fef492098de6fc5a8bccedad2
Matthew Thompson <matthewbot@gmail.com>
2009-07-28 04:00:30 +0000

    Added support for high res timing, includes systick value

--- FC/src/drivers/time.c
+++ FC/src/drivers/time.c
@@ -2,6 +2,7 @@
 #include "peripherals/systick.h"
 #include "peripherals/nvic.h"
 #include "stm32f10x.h"
+#include <stdio.h>
 
 static void systick_irq_handler();
 
@@ -19,6 +20,34 @@ unsigned long time_get() {
 void time_sleep(unsigned long msec) {
 	unsigned long start = time_get();
 	while (time_get() - start < msec) { }
+}
+
+struct time_highres time_get_highres() {
+	struct time_highres ret;
+	
+	ret.cycles = systick_get();
+	ret.msecs = milliseconds;
+	
+	return ret;
+}
+
+struct time_highres time_highres_subtract(struct time_highres timea, struct time_highres timeb) {
+	struct time_highres ret;
+	
+	ret.msecs = timea.msecs - timeb.msecs;
+	
+	if (timea.cycles < timeb.cycles) {
+		timea.cycles += 72000;
+		timea.msecs--;
+	}
+	
+	ret.cycles = timea.cycles - timeb.cycles;
+	
+	return ret;
+}
+
+void time_highres_print(struct time_highres time) {
+	printf("Msec %u Cycles %u\n", (unsigned int)time.msecs, (unsigned int)time.cycles);
 }
 
 static void systick_irq_handler() {
