5a2826e07363d46a15908aaebdfafef22e7ecb51
Matthew Thompson <matthewbot@gmail.com>
2009-08-02 01:22:50 +0000

    Adjusted baud rate, made test program for grapher

--- FC/src/main.c
+++ FC/src/main.c
@@ -1,20 +1,24 @@
 #include "math/matrix.h"
 #include "drivers/micromag.h"
-#include "drivers/vexrc.h"
-#include "drivers/time.h"
-#include "drivers/panel.h"
+#include "drivers/analog.h"
 #include "system.h"
 #include <stdio.h>
 
+static void printmicromag(struct micromag_scan_results results);
+static void printanalog(struct analog_readings readings);
+
 int main(int argc, char **argv) {
 	system_init();
-	printf("Eh?\n");
 
-	micromag_scan(MICROMAG_PERIOD_1024, NULL);
-	
-	for (;;) {
-		time_sleep(500);
-		printf("Micromag ok: %d Vex synced: %d\n", (int)micromag_scan_ok(), (int)vexrc_get_synced());
-	}
+	micromag_scan(MICROMAG_PERIOD_1024, printmicromag);
+	analog_set_update_handler(printanalog);
 }
 
+static void printmicromag(struct micromag_scan_results results) {
+	//printf("%d %d %d\n", (int)results.x, (int)results.y, (int)results.z);
+}
+
+static void printanalog(struct analog_readings readings) {	
+	printf("%u %u %u\n", (unsigned int)readings.roll_rate, (unsigned int)readings.pitch_rate, (unsigned int)readings.yaw_rate);
+	//printf("%u %u %u\n", (unsigned int)readings.x_accel, (unsigned int)readings.y_accel, (unsigned int)readings.z_accel);
+}
