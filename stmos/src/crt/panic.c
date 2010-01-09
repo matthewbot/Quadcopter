28d304cda4c82edea87a899016b35b1be6d0b3bc
Matthew Thompson <matthewbot@gmail.com>
2010-01-09 03:55:02 +0000

    Made panic print the current task

--- stmos/src/crt/panic.c
+++ stmos/src/crt/panic.c
@@ -1,14 +1,29 @@
 #include "panic.h"
 #include <stmos/crt/nvic.h>
 #include <stmos/crt/debug.h>
+#include <stmos/kernel/task.h>
+#include <stmos/kernel/sched.h>
+#include <string.h>
 
 __attribute__((noreturn))
 void panic(const char *msg) {
 	nvic_disable_interrupts();
 	debug_init();
 	
+	struct kernel_task *curtask = sched_get_current_task();
+	
+	static char name[10];
+	memcpy(name, curtask->name, sizeof(name));
+	name[sizeof(name)-1] = '\0';
+	
 	while (true) {
+		debug_print("Panic: ");
 		debug_print(msg);
+		debug_print("\nTask: ");
+		debug_printhex((uint32_t)curtask);
+		debug_print(" ");
+		debug_print(name);
+		debug_print("\n");
 		
 		debug_flashled(1000000);
 		debug_delay(10000000);
