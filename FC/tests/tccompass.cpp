8437b9b9b61ae7958a141a3650687bdadb0c5ed0
Matthew Thompson <matthewbot@gmail.com>
2010-01-09 04:49:12 +0000

    Made tccompass test dump yaw gyro as well for ideal offline kalman filtering

--- FC/tests/tccompass.cpp
+++ FC/tests/tccompass.cpp
@@ -12,24 +12,33 @@ const IOPin::PortPin drdy = { IOPin::PORT_B, 10 };
 MicroMag mag(2, reset, drdy);
 
 const AnalogSensors::Channels chans = { {6, 5, 8, 7, 9, 11} };
-AnalogSensors::AccelerometerCalibration calibrations = {
-      { 32000, -6750 }, // x-axis
+AnalogSensors::Calibrations calibrations = { {
+	{ { 0, 2206 }, // roll
+	  { 0, -2206 }, // pitch
+	  { 0, -4000 } }, // yaw
+	{ { 32000, -6750 }, // x-axis
 	  { 32000, -6750 }, // y-axis
-	  { 32000, 6750 } // z-axis
-};	
+	  { 32000, 6750 } } // z-axis
+} };	
 ADC adc(1);
+AnalogSensors analog(adc, chans, calibrations);
 
-TCCompass compass(chans, calibrations, adc, mag);
+TCCompass compass(chans, calibrations.accel, adc, mag);
 
 USART out(1, 115200);
 
 int main(int argc, char **argv) {
 	out.print("Tilt compensated compass test\n");
 	
-	while (true) {
-		Task::sleep(500);
+	calibrations.gyro.yaw.center = adc.sampleChannel(chans.yaw_gyro);
 	
-		out.printf("Heading: %f\n", compass.readHeading());
+	while (true) {
+		long start = Task::getCurrentTick();
+		
+		AnalogSensors::Readings readings = analog.getReadings();
+		out.printf("%f %f\n", compass.readHeading(), readings.yaw_gyro);
+		
+		Task::sleep(5 - (Task::getCurrentTick() - start));
 	}
 }
 
