48c0f80ab919319d854877ea20e8e99960de3138
Matthew Thompson <matthewbot@gmail.com>
2010-01-10 04:28:45 +0000

    Made panic and debug headers C++ ready

--- stmos/src/crt/debug.h
+++ stmos/src/crt/debug.h
@@ -1,6 +1,10 @@
 #ifndef STMOS_CRT_DEBUG_H
 #define STMOS_CRT_DEBUG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -16,7 +20,10 @@ void debug_print(const char *msg);
 void debug_write(const uint8_t *data, size_t len);
 void debug_printhex(uint32_t num);
 
-
 void debug_delay(unsigned int cycles);
+
+#ifdef __cplusplus
+}
+#endif 
 
 #endif
--- stmos/src/crt/panic.h
+++ stmos/src/crt/panic.h
@@ -1,7 +1,15 @@
 #ifndef STMOS_CRT_PANIC_H
 #define STMOS_CRT_PANIC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
 __attribute__((noreturn))
 void panic(const char *msg);
+
+#ifdef __cplusplus
+}
+#endif 
 
 #endif
