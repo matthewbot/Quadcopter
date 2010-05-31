4729ec58a4498dfba61999d19fa9f3bc60f70359
Matthew Thompson <matthewbot@gmail.com>
2010-05-31 10:38:28 -0400

    Rewrote SPI peripheral to put the calling process to sleep and use interrupts

--- stmos/src/peripherals/SPI.cpp
+++ stmos/src/peripherals/SPI.cpp
@@ -1,4 +1,6 @@
 #include "SPI.h"
+#include <stmos/util/Lock.h>
+#include <stmos/crt/nvic.h>
 #include <stm32f10x.h>
 
 using namespace stmos;
@@ -7,6 +9,14 @@ static const IOPinConfig::Port spiports[2] = { IOPinConfig::PORT_A, IOPinConfig:
 static const IOPinConfig::Pin spibasepins[2] = { 4, 12 };
 static SPI_TypeDef *const spis[] = { 0, SPI1, SPI2 };
 
+static SPI *objs[2];
+template <int i> static void irq_callback() {
+	objs[i]->irq();
+}
+static void (*const irq_callbacks[])() = {irq_callback<0>, irq_callback<1>};
+
+#define IRQ(num) (SPI1_IRQn + (num) - 1)
+
 SPI::SPI(int num, Baud baud)
 : num(num),
   nss_pin(spiports[num-1], spibasepins[num-1], IOPinConfig::OUTPUT, IOPinConfig::NONE, true),
@@ -14,6 +24,8 @@ SPI::SPI(int num, Baud baud)
   miso_pin(spiports[num-1], spibasepins[num-1]+2, IOPinConfig::INPUT, IOPinConfig::NONE, true),
   mosi_pin(spiports[num-1], spibasepins[num-1]+3, IOPinConfig::OUTPUT, IOPinConfig::NONE, true) 
 {
+	objs[num-1] = this;
+	
  	switch (num) {
  		case 1:
  			RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
@@ -29,24 +41,65 @@ SPI::SPI(int num, Baud baud)
 	spi->CR1 = 0;
 	spi->CR2 = SPI_CR2_SSOE;
 	spi->CR1 = SPI_CR1_SPE | SPI_CR1_MSTR | (baud << 3);
+	
+	int irq = IRQ(num);
+	nvic_set_handler(irq, irq_callbacks[num-1]);
+	nvic_set_enabled(irq, true);
 }
 
-void SPI::sendReceive(const uint8_t *sendbuf, uint8_t *receivebuf, size_t len) {
+SPI::~SPI() {
+	nvic_set_enabled(IRQ(num-1), false);
+}
+
+void SPI::sendReceive(const uint8_t *sendbuf, uint8_t *recvbuf, size_t len) {
+	SPI_TypeDef *spi = spis[num];
+
+	Lock lock(mutex);
+	CriticalSection crit(IRQ(num));
+	
+	this->sendbuf = sendbuf;
+	this->sendlen = len;
+	this->recvbuf = recvbuf;
+	this->recvlen = len;
+
+	spi->CR2 |= SPI_CR2_TXEIE | SPI_CR2_RXNEIE;
+	notifier.waitLeave(crit);
+}
+
+#include <stmos/crt/debug.h>
+
+void SPI::irq() {
 	SPI_TypeDef *spi = spis[num];
 	
-	while (len--) {
-		while (!(spi->SR & SPI_SR_TXE)) { }
+	uint32_t sr = spi->SR;
+
+	if ((sr & SPI_SR_RXNE) && recvlen) {
+		if (recvbuf) {
+			*recvbuf++ = spi->DR;
+		} else {
+			(void)spi->DR;
+		}
+		
+		if (--recvlen == 0) {
+			spi->CR2 &= ~SPI_CR2_RXNEIE;
+			register_irqcallback();
+		}
+	}
 	
-		if (sendbuf)
+	if ((sr & SPI_SR_TXE) && sendlen) {
+		if (sendbuf) {
 			spi->DR = *sendbuf++;
-		else
+		} else {
 			spi->DR = 0;
+		}
 		
-		while (!(spi->SR & SPI_SR_RXNE)) { }
-		uint32_t got = spi->DR;
-		
-		if (receivebuf) 
-			*receivebuf++ = got;
+		if (--sendlen == 0)
+			spi->CR2 &= ~SPI_CR2_TXEIE;
 	}
 }
+
+void SPI::irqcallback() {
+	notifier.notify();
+}
+
 
--- stmos/src/peripherals/SPI.h
+++ stmos/src/peripherals/SPI.h
@@ -3,10 +3,13 @@
 
 #include "IOPin.h"
 #include <stmos/util/NonCopyable.h>
+#include <stmos/util/IRQCallback.h>
+#include <stmos/util/Mutex.h>
+#include <stmos/util/Notifier.h>
 #include <cstddef>
 
 namespace stmos {
-	class SPI : NonCopyable {
+	class SPI : IRQCallback, NonCopyable {
 		public:
 			enum Baud {
 				BAUDDIV_2,
@@ -20,13 +23,25 @@ namespace stmos {
 			};
 		
 			SPI(int num, Baud baud);
+			~SPI();
 
-			void sendReceive(const uint8_t *sendbuf, uint8_t *receivebuf, size_t len);			
+			void sendReceive(const uint8_t *sendbuf, uint8_t *recevbuf, size_t len);			
 			inline void send(const uint8_t *sendbuf, size_t len) { sendReceive(sendbuf, NULL, len); }
-			inline void receive(uint8_t *receivebuf, size_t len) { sendReceive(NULL, receivebuf, len); }
+			inline void receive(uint8_t *recvbuf, size_t len) { sendReceive(NULL, recvbuf, len); }
+			
+			void irq();
+			virtual void irqcallback();
 			
 		private:
 			int num;
+			
+			const uint8_t *sendbuf;
+			size_t sendlen;
+			uint8_t *recvbuf;
+			size_t recvlen;
+			
+			Mutex mutex;
+			Notifier notifier;
 			
 			IOPinConfig nss_pin, sck_pin, miso_pin, mosi_pin;
 	};
