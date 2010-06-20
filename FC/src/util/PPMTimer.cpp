862e523c71bd1e26f97838d7c48d57fb1a488127
Matthew Thompson <matthewbot@gmail.com>
2010-06-19 23:23:00 -0400

    Increased IRQ priority of the PPM timer in an attempt to solve apparent transmitter glitches

--- FC/src/util/PPMTimer.cpp
+++ FC/src/util/PPMTimer.cpp
@@ -3,7 +3,7 @@
 using namespace FC;
 using namespace stmos;
 
-PPMTimer::PPMTimer() : Timer(4) {
+PPMTimer::PPMTimer() : Timer(4, IRQ_PRIORITY_MED) {
 	setTickTime(72);
 	setOverflow(40000);
 	start();
