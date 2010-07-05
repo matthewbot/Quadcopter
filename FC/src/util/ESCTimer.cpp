d0460febc09d4f9433d0feff9954b3a7c8992ed7
Matthew Thompson <matthewbot@gmail.com>
2010-07-04 22:15:23 -0400

    Elevated the ESC timer's interrupt priority

--- FC/src/util/ESCTimer.cpp
+++ FC/src/util/ESCTimer.cpp
@@ -3,7 +3,7 @@
 using namespace FC;
 using namespace stmos;
 
-ESCTimer::ESCTimer() : Timer(3) {
+ESCTimer::ESCTimer() : Timer(3, IRQ_PRIORITY_MED+1) {
 	setTickTime(72);
 	setOverflow(20000);
 	start();
